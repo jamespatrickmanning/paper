b66805ec034762dedda32f00d434b8571b9d8b61
Semi-Annual <55396095+Mingchao19960125@users.noreply.github.com>
2020-03-27 03:29:15 +0800

    Add files via upload

--- py/thenumberofprofiles_fishingVSship_permonth.py
+++ py/thenumberofprofiles_fishingVSship_permonth.py
@@ -51,7 +51,7 @@ fishing_path = 'E:\\Mingchao\\result\\emolt_dat_all(only emolt_no_telemetry).csv
 ship_path = 'E:\\Mingchao\\parameter\\original_ship_data.csv'
 permonth_save = 'E:\\Mingchao\\result\\'
 end_time = datetime.utcnow()
-start_str = '2015-6-10 00:00:00'
+start_str = '2015-1-01 00:00:00'
 start_time = datetime.strptime(start_str, '%Y-%m-%d %H:%M:%S')
 #main
 fishing_df = read_emolt(start=start_time, end=end_time, path='https://www.nefsc.noaa.gov/drifter/emolt.dat')
@@ -65,7 +65,8 @@ ship_df['day'] = pd.to_datetime(ship_df['day'])
 #fper month
 ship_Num=[]
 fishing_Num=[]
-for i in range(6):    # 2014~2018,5 years
+#for i in range(6):    # 2014~2018,5 years
+for i in range(4):
     #ship_Num.append([0]*12)    #12 months
     fishing_Num.append([0]*12)
 for i in range(4):
@@ -79,41 +80,52 @@ for i in range(len(ship_df['day'])):
                     ship_Num[j][q]+=1
 
 for i in range(len(fishing_df)):
-    for j in range(6):
+    #for j in range(6):
+    for j in range(4):
         if fishing_df['time'][i].year == 2015+j:
             for q in range(12):
                 if fishing_df['time'][i].month == q+1:
                     fishing_Num[j][q]+=1
 #plot
 width=0.2
-color=['blue','black','red','green','yellow','gray']
+#color=['blue','black','red','green','yellow','gray']
+color=['blue','red','green','yellow']
 fig=plt.figure()
 ax1 = fig.add_subplot(1,2,1)
-for i in range(6):
-    ax1.bar(np.arange(1,13)+width*(i-3.75),fishing_Num[i],align="center",width=width,color=color[i],label=str(i+2015))
+#for i in range(6):
+for i in range(4):
+    #ax1.bar(np.arange(1,13)+width*(i-3.75),fishing_Num[i],align="center",width=width,color=color[i],label=str(i+2015))
+    ax1.bar(np.arange(1,13)+width*(i-1.75),fishing_Num[i],align="center",width=width,color=color[i],label=str(i+2015))
 plt.legend(loc='best',fontsize = 'x-small')
 plt.xlim([0,13]) 
-plt.ylim([0,600])
-plt.xticks(range(13),fontsize=10)
+#plt.ylim([0,600])
+plt.ylim([0,400])
+#plt.xticks(range(13),fontsize=10)
+plt.xticks(np.arange(1,13))
 plt.yticks(fontsize=10)
-plt.ylabel('Quantity',fontsize=16)
-plt.title('#Fishing profiles per month',fontsize=12)
+#plt.ylabel('Quantity',fontsize=16)
+plt.ylabel('number of bottom temperature observations')
+#plt.title('#Fishing profiles per month',fontsize=12)
+plt.title('Fishing Vessels',fontsize=12)
 
 ax2 = fig.add_subplot(1,2,2)
 for i in range(4):
-    ax2.bar(np.arange(1,13)+width*(i-3.75),ship_Num[i],align="center",width=width,color=color[i] ,label=str(i+2015))
+    #ax2.bar(np.arange(1,13)+width*(i-3.75),ship_Num[i],align="center",width=width,color=color[i] ,label=str(i+2015))
+    ax2.bar(np.arange(1,13)+width*(i-1.75),ship_Num[i],align="center",width=width,color=color[i] ,label=str(i+2015))
 plt.legend(loc='best',fontsize = 'x-small')
-plt.xlim([1,13]) 
-plt.ylim([0,600])
-plt.xticks(np.arange(1,13),fontsize=10)
+plt.xlim([0,13]) 
+#plt.ylim([0,600])
+plt.ylim([0,400])
+#plt.xticks(range(13),fontsize=10)
+plt.xticks(np.arange(1,13))
 plt.setp(ax2.get_yticklabels(),visible=False)
 fig.text(0.5,0.04,'Month',ha='center', va='center',fontsize=16)
-plt.title('#Ship profiles per month',fontsize=12)
-plt.show()
+#plt.title('#Ship profiles per month',fontsize=12)
+plt.title('Research Vessels',fontsize=12)
 if not os.path.exists(permonth_save):
         os.makedirs(permonth_save)
 plt.savefig(permonth_save+'fishingVSship_profiles.png', dpi=200)
-
+plt.show()
 
 
 
